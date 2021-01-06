22f97dbbf8eba0d94eb2edcce3e1ebf735e0fa39
Felipe Jimenez Quiros <fjimenezq@bncr.fi.cr>
2021-01-05 20:51:15 -0600

    Paso a lab 3

---
+++ entregables/labs/lab-03/ProductsApi/Controllers/ProductsController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProductsApi.Models;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductsApi.Controllers
+{
+  [Route("api/v1/[controller]")]
+  [ApiController]
+  public class ProductsController : ControllerBase
+  {
+
+    private readonly ILogger<ProductsController> _logger;
+    //private readonly AdventureWorksDbContext _context;
+    private readonly ProductRepository _productRepository;
+
+    public ProductsController(ILogger<ProductsController> logger
+      , ProductRepository productRepository
+        //, AdventureWorksDbContext context
+        )
+    {
+      _logger = logger;
+      //_context = context; 
+      _logger.LogInformation("Constructor products");
+      _productRepository = productRepository;
+    }
+
+    // GET: api/<ProductsController>
+    [HttpGet]
+    public IEnumerable<Product> Get()
+    {
+      return _productRepository.Get();
+    }
+
+    // GET api/<ProductsController>/5
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+      var result = _productRepository.Get(id);
+      if (result == null)
+        return NotFound();
+
+      return Ok(result);
+    }
+
+    // POST api/<ProductsController>
+    [HttpPost]
+    public IActionResult Post([FromBody] Product value)
+    {
+      Product result = _productRepository.Add(value);
+      return CreatedAtAction(nameof(GetById), new { Id = result.Id }, value);
+    }
+
+    // PUT api/<ProductsController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<ProductsController>/5
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+      var result = _productRepository.Get(id);
+      if (result == null)
+        return NotFound();
+
+      _productRepository.Delete(id);
+      return new ObjectResult(new object()) { StatusCode = (int)HttpStatusCode.NotImplemented };
+    }
+  }
+}
---
+++ entregables/labs/lab-03/ProductsApi/Repositories/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using Core.Data;
+using ProductsApi.Models;
+using System.Linq;
+
+namespace Repositories
+{
+  public class ProductRepository
+  {
+    private readonly AdventureWorksDbContext _context;
+    public ProductRepository(AdventureWorksDbContext context)
+    {
+      _context = context;      
+    }
+
+    public Product[] Get()
+    {
+      return _context.Products.ToArray();
+    }
+
+    public Product Add(Product value)
+    {
+      _context.Products.Add(value);
+      _context.SaveChanges();
+      return value;
+    }
+
+    public void Delete(int id)
+    {
+      var match = _context.Products.Find(id);
+      if (match != null)
+      {
+        _context.Remove(match);
+        _context.SaveChanges();
+      }
+    }
+
+    public Product Get(int id)
+    {
+      return _context.Products.Find(id);
+    }
+  }
+}
